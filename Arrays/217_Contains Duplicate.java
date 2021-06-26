e311653fb4b3881a26fb7e59a0e0534cfd2b0c19
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-06-27 00:16:13 +0530

    Update and rename 217_Contains Duplicate to 217_Contains Duplicate.java

--- Arrays/217_Contains Duplicate.java
+++ Arrays/217_Contains Duplicate.java
@@ -1,22 +1,6 @@
-Example 1:
-
-Input: nums = [1,2,3,1]
-Output: true
-Example 2:
-
-Input: nums = [1,2,3,4]
-Output: false
-Example 3:
-
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-Output: true
-
-
 TC:  O(n)
 SC:  O(n)
 
-
-Code:
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
