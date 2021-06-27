12fa91b847a12f726facf4f30c4d7fd7128dfff6
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-06-28 00:56:47 +0530

    Update 448_Find All Numbers Disappeared in an Array.java

--- Arrays/448_Find All Numbers Disappeared in an Array.java
+++ Arrays/448_Find All Numbers Disappeared in an Array.java
@@ -1,3 +1,11 @@
+As we walk through all elements in the array, we can use the current element as an index and flag its value (in place). All numbers are > 0, so we can use a negative number.
+In the end, the indexes of all positive numbers in the array are the missing values.
+
+The input: [4, 3, 2, 7, 8, 2, 3, 1]
+Becomes: [-4, -3, -2, -7, 8, 2, -3, -1]
+
+Starting from 1, the missing values are 5 and 6.
+        
 TC: O(n);
 SC: O(1);
 
