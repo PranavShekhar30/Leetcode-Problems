271a3ff9a00de988dbe578819bc006380e87d012
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-06-29 22:48:39 +0530

    Update 136_Single Number.java

--- Arrays/136_Single Number.java
+++ Arrays/136_Single Number.java
@@ -1,1 +1,14 @@
+TC: O(n);
+SC: O(1);
 
+Using XOR as xor of 2 same numbers gives 0(2^2=0) and xor of 2 numbers (x^0=x).
+  
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
