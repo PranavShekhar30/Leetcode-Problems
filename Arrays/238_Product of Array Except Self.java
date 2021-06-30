3ff73e0d66f82a910a5ace1248451604e8526498
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-01 00:00:28 +0530

    Update 238_Product of Array Except Self.java

--- Arrays/238_Product of Array Except Self.java
+++ Arrays/238_Product of Array Except Self.java
@@ -1,4 +1,20 @@
 TC: O(n)
 SC: O(1);
 
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int ans[] = new int[len];
+        ans[0] = 1;
+        for(int i=1;i<nums.length;i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        int right = 1;
+        for(int i=len-1;i>=0;i--){
+            ans[i] = ans[i] * right;
+            right = right * nums[i];
+        }
+        return ans;
+    }
+}
 
