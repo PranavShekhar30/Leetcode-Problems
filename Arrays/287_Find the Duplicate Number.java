4a3ebbf6233cbf6373fd2c7ad4c42f6d07bf07ee
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-01 11:18:07 +0530

    Update 287_Find the Duplicate Number.java

--- Arrays/287_Find the Duplicate Number.java
+++ Arrays/287_Find the Duplicate Number.java
@@ -1,1 +1,47 @@
+TC: O(N)
+SC: O(1)
+  
+Floyd's Tortoise and Hare (Cycle Detection)
+  
+  class Solution {
+       public int findDuplicate(int[] nums) {
+        int fastPointer = 0, slowPointer = 0;
+        
+		// Phase 1: Try to make them meet each other
+    do
+        {
+            fastPointer = nums[nums[fastPointer]];
+            slowPointer = nums[slowPointer];
+        }
+        while (fastPointer != slowPointer);
+        
+	// Phase 2: Bring the slow pointer to the start and make them move over 1 by 1
+        
+           slowPointer = 0;
+        while (slowPointer != fastPointer) {
+            slowPointer = nums[slowPointer];
+            fastPointer = nums[fastPointer];
+        }
+        
+        return slowPointer;
+    }
 
+}
+
+OR
+
+TC: O(nlogn)
+SC: O(1) or O(n)
+  
+  class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int ans=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                ans = nums[i];
+            }
+        }
+        return ans;
+    }
+}
