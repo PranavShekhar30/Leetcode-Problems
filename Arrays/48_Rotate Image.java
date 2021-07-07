1ed6054e56fc1f9e352b65f6d94c6635a69d5bc0
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-08 00:13:59 +0530

    Update 48_Rotate Image.java

--- Arrays/48_Rotate Image.java
+++ Arrays/48_Rotate Image.java
@@ -7,7 +7,7 @@ class Solution {
         // First we transpose the matrix
         
         for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix.length;j++){
+            for(int j=i;j<matrix[0].length;j++){
                 int temp = 0;
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
