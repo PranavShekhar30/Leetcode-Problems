e8139afbaa4523a203273d1c1f7a382835214df4
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-07 21:30:16 +0530

    Create 54_Spiral Matrix.java

---
+++ Arrays/54_Spiral Matrix.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return list;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int top = 0;
+        int left = 0;
+        int down = m-1;
+        int right = n-1;
+        
+      while(true){
+          
+          for(int i=left;i<=right;i++){
+              list.add(matrix[top][i]);
+          }
+          top++;
+           if(top > down){
+               break;
+           }
+          for(int i=top;i<=down;i++){
+              list.add(matrix[i][right]);
+          }
+          right--;
+          if(right < left){
+              break;
+          }
+          for(int i=right;i>=left;i--){
+              list.add(matrix[down][i]);
+          }
+           down--; 
+           if(down < top){
+               break;
+           }
+          for(int i=down;i>=top;i--){
+              list.add(matrix[i][left]);  
+          }
+        left++;
+           if(left > right) {
+               break;
+           }
+          
+      }
+  
+        return list;
+    }
+}
