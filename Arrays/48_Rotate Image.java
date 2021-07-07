bb07e7542f50351cebcb55b31836adfcedf14536
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-08 00:03:09 +0530

    Update 48_Rotate Image.java

--- Arrays/48_Rotate Image.java
+++ Arrays/48_Rotate Image.java
@@ -1,1 +1,30 @@
+TC: O(N^2);
+SC: O(1);
 
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        // First we transpose the matrix
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix.length;j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Rotate all the rows
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length/2;j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+
+    }
+}
