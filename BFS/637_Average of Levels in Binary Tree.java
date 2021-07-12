3823314fba1dc531ef02699605efa4f70a774fea
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-12 23:26:27 +0530

    Update 637_Average of Levels in Binary Tree.java

--- BFS/637_Average of Levels in Binary Tree.java
+++ BFS/637_Average of Levels in Binary Tree.java
@@ -1,1 +1,26 @@
+TC: O(N);
+SC: O(N);
 
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> avg = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+       while(!q.isEmpty()){
+           int size = q.size();
+           long sum = 0;
+           for(int i=0;i<size;i++){
+               TreeNode curr = q.poll();
+               sum+=curr.val;
+               if(curr.left != null){
+                   q.add(curr.left);
+               }
+                if(curr.right != null){
+                   q.add(curr.right);
+               }
+           }  
+           avg.add((double)sum/size);
+       }
+        return avg;
+    }
