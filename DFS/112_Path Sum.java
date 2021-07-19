0e61ce09e5a3b2ab3586737a69677afb1b1494e8
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-20 00:26:21 +0530

    Create 112_Path Sum.java

---
+++ DFS/112_Path Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+       if((root.left == null && root.right == null) && targetSum - root.val == 0){
+           return true;
+       }
+        
+        boolean flag1 = hasPathSum(root.left, targetSum - root.val);
+        boolean flag2 = hasPathSum(root.right, targetSum - root.val);
+            
+       return flag1 || flag2;
+    }
+}
