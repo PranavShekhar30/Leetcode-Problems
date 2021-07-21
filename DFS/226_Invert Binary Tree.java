e4015a09535230bc25067f3372871fe2900f51e7
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-21 23:19:01 +0530

    Update 226_Invert Binary Tree.java

--- DFS/226_Invert Binary Tree.java
+++ DFS/226_Invert Binary Tree.java
@@ -1,3 +1,7 @@
+TC: O(N);
+SC: O(N);   {Because of recursion, O(h) function calls will be placed on the stack in the worst case, where h is the height of the tree. Because h ∈ O(n), the space complexity is O(n)}
+
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root==null){
