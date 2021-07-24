af6d18bff0738e6f668790b8df361e59da175c9d
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-25 01:13:36 +0530

    Create 98_Validate Binary Search Tree.java

---
+++ DFS/98_Validate Binary Search Tree.java
@@ -0,0 +1,20 @@
+TC: O(N);
+SC: O(N);
+
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root,null,null);
+    }
+    
+    public boolean helper(TreeNode root, Integer min, Integer max){
+   
+        if(root==null){
+            return true;
+        }
+        if((min != null && root.val <= min) || (max != null && root.val >= max)){
+            return false;
+        }
+        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+    }
+}
