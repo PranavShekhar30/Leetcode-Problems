56c3b40c9808f2872f5d19556cce9bfc51c7958d
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-26 23:48:50 +0530

    Update 121_Best Time to Buy and Sell Stock.java

--- Dynamic Programming/121_Best Time to Buy and Sell Stock.java
+++ Dynamic Programming/121_Best Time to Buy and Sell Stock.java
@@ -18,3 +18,27 @@ class Solution {
         return profit;
     }
 }
+
+
+Method - 2: Using DP
+
+
+TC: O(N);
+SC: O(1);
+class Solution {
+    public int maxProfit(int[] prices) {
+ 
+        int n = prices.length;
+        int[] dp = new int[n];
+        
+        dp[0]=0;
+        int minValue = prices[0];
+        for(int i=1; i<n; i++){
+            dp[i] = Math.max(dp[i-1],prices[i] - minValue);
+            minValue = Math.min(minValue,prices[i]);
+        }
+        return dp[n-1];
+    }
+}
+
+
