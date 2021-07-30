9ed57e1e070febeac9febcd76de1791eef869fa1
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-31 00:55:16 +0530

    Update 300_Longest Increasing Subsequence.java

--- Dynamic Programming/300_Longest Increasing Subsequence.java
+++ Dynamic Programming/300_Longest Increasing Subsequence.java
@@ -72,19 +72,18 @@ BST Implementation using TreeSet (Java Provides BST implementation)
     
   // TC: O(nlogn)
   // SC: O(n)
-  public int lengthOfLIS(int[] nums) {
-    TreeSet<Integer> bst = new TreeSet<>();
-    for (int num : nums) {
-      Integer higherOrEqual = bst.ceiling(num);
-      if (higherOrEqual == null) {
-        bst.add(num);
-      }
-      else {
-        bst.remove(higherOrEqual);
-        bst.add(num); // O(logn)
-      }
+ class Solution {
+    public int lengthOfLIS(int[] nums) {
+        TreeSet<Integer> bst = new TreeSet<>();
+        for(int i : nums){
+            Integer High_orEqual = bst.ceiling(i);
+            if(High_orEqual == null){
+                bst.add(i);
+            }else{
+                bst.remove(High_orEqual);
+                bst.add(i);
+            }
+        }
+        return bst.size();
     }
-
-    return bst.size();
-  }
 }
