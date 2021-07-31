3ef7f397291b4a4a8ae83016ed6f32b59c3338db
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-07-31 14:59:48 +0530

    Create 5_Longest Palindromic Substring.java

---
+++ Dynamic Programming/5_Longest Palindromic Substring.java
@@ -0,0 +1,35 @@
+TC: O(N^2);
+SC: O(1)
+  
+class Solution {
+    public String longestPalindrome(String s) {
+       if(s == null && s.length() < 1){
+           return "";
+       }
+        int start = 0;
+        int end = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            int len1 = expandAroundMiddle(s,i,i);
+            int len2 = expandAroundMiddle(s,i,i+1);
+            int len = Math.max(len1,len2);
+            if(len > end - start){
+                start = i - ((len - 1)/2);
+                end = i + (len/2);
+            }
+           
+        }
+        return s.substring(start,end + 1);
+    }
+    
+    public int expandAroundMiddle(String s, int left, int right){
+        if(s == null || left > right){
+            return 0;
+        }
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
