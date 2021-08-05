edb14240de6503931cfee87d07443829992cf31a
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-06 00:39:30 +0530

    Create 746_Min Cost Climbing Stairs.java

---
+++ Dynamic Programming/746_Min Cost Climbing Stairs.java
@@ -0,0 +1,18 @@
+TC: O(N);
+SC: O(N);
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost.length < 2){
+            return 0;
+        }
+        int n = cost.length;
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2; i <= n; i++){
+            dp[i] = Math.min(dp[i-1] + cost[i-1] , dp[i-2] + cost[i-2]);
+        }
+        return dp[n];
+    }
+}
