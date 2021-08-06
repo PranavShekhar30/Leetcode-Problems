fb4e68947b21d83ab237112e71159c7f7031ff1b
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-06 12:08:10 +0530

    Update 1646_Get Maximum in Generated Array.java

--- Dynamic Programming/1646_Get Maximum in Generated Array.java
+++ Dynamic Programming/1646_Get Maximum in Generated Array.java
@@ -11,6 +11,34 @@ class Solution {
         dp[0] = 0;
         dp[1] = 1;
         
+        for(int i=1; i<=n; i++){
+            if(2*i <= n){
+                dp[2*i] = dp[i];
+                max = Math.max(max,dp[2*i]);
+            }
+            if(2*i + 1 <= n){
+                dp[2*i + 1] = dp[i] + dp[i + 1];
+                max = Math.max(max,dp[2*i + 1]);
+            }
+        }
+        return max;
+    }
+}
+ 
+
+OR
+
+
+class Solution {
+    public int getMaximumGenerated(int n) {
+        if(n < 2){
+            return n;
+        }
+        int dp[] = new int[n + 1];
+        int max = -1;
+        dp[0] = 0;
+        dp[1] = 1;
+        
         for(int i=2; i<=n; i++){
             if(i % 2 == 0){
                 dp[i] = dp[i/2];
