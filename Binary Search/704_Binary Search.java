d183427e828dd834947f7153331fd38c518d195a
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-08 00:59:50 +0530

    Create 704_Binary Search.java

---
+++ Binary Search/704_Binary Search.java
@@ -0,0 +1,40 @@
+Using Linear Search:
+
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+OR
+
+
+Using Binary Search
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        
+        while(start <= end){
+            int mid = (start + end)/2;
+            
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }
+            else{
+               start = mid + 1; 
+            }
+        }
+        return -1;
+    }
+}
