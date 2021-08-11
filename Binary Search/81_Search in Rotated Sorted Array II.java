fd62f0f9f4300cf8144b06b088e13fcd710caf7e
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-11 23:43:46 +0530

    Update 81_Search in Rotated Sorted Array II.java

--- Binary Search/81_Search in Rotated Sorted Array II.java
+++ Binary Search/81_Search in Rotated Sorted Array II.java
@@ -10,10 +10,19 @@ class Solution {
                 return true;
             }
             else if(target > nums[left]){
+                while(left < right && nums[left] == nums[left+1]){   //To skip duplicates
+                    left++;
+                }
                 left++;
             }
             else if(target < nums[right]){
+                while(left < right && nums[right] == nums[right-1]){    //To skip duplicates
+                    right--;
+                }
                 right--;
+            }
+            else{
+                break;
             }
         }
         return false;
