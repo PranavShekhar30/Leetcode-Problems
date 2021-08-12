da7aa2320f8126b944d6c34f95a2daf14a1fa56d
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-12 23:20:05 +0530

    Update 74_Search a 2D Matrix.java

--- Binary Search/74_Search a 2D Matrix.java
+++ Binary Search/74_Search a 2D Matrix.java
@@ -17,3 +17,35 @@ class Solution {
         return false;
     }
 }
+
+
+Method -2: Using Binary Search
+
+TC: O(log(n*m);
+SC: O(1);
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0){
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int start = 0;
+        int end = n*m-1;
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            int mid_element = matrix[mid/n][mid%n];
+            if(target == mid_element){
+                return true;
+            }else if(target < mid_element){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+}
