2395d327d2cb9918418ace76f80839ed179bc564
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-18 23:18:22 +0530

    Update 784_Letter Case Permutation.java

--- Backtracking/784_Letter Case Permutation.java
+++ Backtracking/784_Letter Case Permutation.java
@@ -1,1 +1,27 @@
-
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> ans = new ArrayList<>();
+        if(s.length() == 0){
+            return ans;
+        }
+        helper(s.toCharArray(), ans, 0);
+        return ans;
+    }
+    
+    public void helper(char[] str, List<String> ans, int index){
+        if(index == str.length){
+            ans.add(new String(str));
+            return;
+        }
+        if(Character.isDigit(str[index])){
+            helper(str, ans, index + 1);
+        }
+        else{
+            str[index] = Character.toLowerCase(str[index]);
+            helper(str, ans, index + 1);
+            
+            str[index] = Character.toUpperCase(str[index]);
+            helper(str, ans, index + 1);
+        }
+    }
+}
