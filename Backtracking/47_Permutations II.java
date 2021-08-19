d8c90bd97a3ee5448bac6e98651cc5bd931ec7c8
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-20 01:25:59 +0530

    Create 47_Permutations II.java

---
+++ Backtracking/47_Permutations II.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        backtracking(ans, nums, 0);
+        return ans;
+    }
+    
+    public void backtracking(List<List<Integer>> ans, int[] nums, int start){
+        if(start == nums.length){
+            ans.add(toList(nums));
+        }
+        else{
+            for(int i = start; i < nums.length; i++){
+                if(i != start && !canPermutate(start, i, nums)){
+                    continue;
+                }
+                swap(i, start, nums);
+                backtracking(ans, nums, start+1);
+                swap(i, start, nums);
+            }
+        }
+    }
+    public List<Integer> toList(int[] nums){
+        List<Integer> res = new ArrayList<>();
+        for(int i: nums){
+            res.add(i);
+        }
+        return res;
+    }
+    
+    public void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public boolean canPermutate(int start, int curr, int[] nums){
+        for(int i = start; i < curr; i++){
+            if(nums[i] == nums[curr]){
+                return false;
+            }
+        }
+         return true;
+    }
+}
+
+    
