8472ac9713788954f426987e8f6af983341d90e7
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-08-24 12:54:27 +0530

    Update 37_Sudoku Solver.java

--- Backtracking/37_Sudoku Solver.java
+++ Backtracking/37_Sudoku Solver.java
@@ -43,3 +43,68 @@ class Solution {
         return true;
     }
 }
+
+
+class Solution {
+    
+    public void solveSudoku(char[][] board) {
+        backtracking(board);
+    }
+    
+    public boolean backtracking(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                
+                // If cell is not empty, skip
+                if (board[i][j] != '.') continue;
+                
+                // For every empty cell get arrayList of values it can be 
+                ArrayList<Character> arr = validValues(board, i, j);
+                
+                // Loop through the valid values
+                for (char c : arr) {
+                    board[i][j] = c;
+                    
+                    // If true, then solved
+                    if (backtracking(board)) {
+                        return true;
+                    }
+                    // else, backtrack
+                    else {
+                        board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        } 
+        return true;
+    }
+    
+    // Returns a arraylist of possible values
+    public ArrayList<Character> validValues(char[][] board, int a, int b) {
+        
+        // Declare all possible values
+        ArrayList<Character> arr = new ArrayList<Character>(Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9'));
+        
+        // Target value
+        char target = board[a][b];
+        
+        // Remove row and column values
+        for (int i = 0; i < 9; i++) {
+            arr.remove(Character.valueOf(board[a][i]));
+            arr.remove(Character.valueOf(board[i][b]));
+        }
+        
+        // Get begining cell of the local 3x3 grid
+        int row = (a / 3) * 3, col = (b / 3) * 3;
+        
+        // Remove 3x3 grid values
+        for (int i = row; i < row+3; i++) {
+           for (int j = col; j < col+3; j++) {
+                arr.remove(Character.valueOf(board[i][j]));
+            } 
+        }
+        
+        return arr;
+    }
+}
