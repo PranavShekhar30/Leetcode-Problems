1c856850298181c6d9e7f5711baa87401bb26c08
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-09-28 22:46:59 +0530

    Update 217_Contains Duplicate.java

--- Arrays/217_Contains Duplicate.java
+++ Arrays/217_Contains Duplicate.java
@@ -20,3 +20,16 @@ class Solution {
     }
 }
 
+OR 
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1;i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
