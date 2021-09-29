ba2cd367a5f5b1f9284ceeca6a5289a8ad3c96ea
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-09-29 23:06:34 +0530

    Update 448_Find All Numbers Disappeared in an Array.java

--- Arrays/448_Find All Numbers Disappeared in an Array.java
+++ Arrays/448_Find All Numbers Disappeared in an Array.java
@@ -52,6 +52,24 @@ class Solution {
 
 OR
 
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=0; i<nums.length; i++){
+            int curr = Math.abs(nums[i]);
+            nums[curr-1] = -(Math.abs(nums[curr -1]));
+        }
+         for(int i=0; i<nums.length; i++){
+            if(nums[i] > 0){
+            list.add(i+1);
+            }
+        }
+        return list;
+    }
+}
+
+OR
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> list = new ArrayList<Integer>();
