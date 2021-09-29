edca5a0385fa0b3f2af73b277b0d234398ab0cf4
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-09-29 22:23:44 +0530

    Update 448_Find All Numbers Disappeared in an Array.java

--- Arrays/448_Find All Numbers Disappeared in an Array.java
+++ Arrays/448_Find All Numbers Disappeared in an Array.java
@@ -49,3 +49,22 @@ class Solution {
         return list;
     }
 }
+
+OR
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i: nums){
+            set.add(i);
+        }
+        for(int i=1; i<=nums.length; i++){
+           if(!set.contains(i)){
+           list.add(i);
+           }  
+        }
+       
+        return list;
+    }
+}
