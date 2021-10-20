48638db1b8ab804f48d4e4d66839085320095b4c
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-10-21 00:50:16 +0530

    Update 1189. Maximum Number of Balloons.java

--- Strings/1189. Maximum Number of Balloons.java
+++ Strings/1189. Maximum Number of Balloons.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        
+        int[] char_counts = new int[26];
+        for(int i=0; i<text.length(); i++){
+            char_counts[text.charAt(i) - 'a']++;           
+        }
+        
+        int min = char_counts[1];                   //b
+        min = Math.min(min, char_counts[0]);        //a
+        min = Math.min(min, char_counts[11] / 2);   //l
+        min = Math.min(min, char_counts[14]) / 2;   //o
+        min = Math.min(min, char_counts[13]);       //n
+        return min;
+        
+    }
+}
 
+     
