5080585c4a64be8ab5c0e52674ea6c5374e295a3
Pranav Shekhar <66740189+PranavShekhar30@users.noreply.github.com>
2021-11-02 08:39:57 -0700

    Update 1046_Last Stone Weight.java

--- Arrays/1046_Last Stone Weight.java
+++ Arrays/1046_Last Stone Weight.java
@@ -2,18 +2,16 @@ TC: O(N*LogN);
 
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        if(stones.length == 0){
-            return 0;
-        }
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         for(int stone : stones){
             maxHeap.add(stone);
         }
+        
         while(maxHeap.size() > 1){
-            int stone1 = maxHeap.poll();
-            int stone2 = maxHeap.poll();
+            int stone1 = maxHeap.remove();
+            int stone2 = maxHeap.remove();
             maxHeap.add(Math.abs(stone1 - stone2));
         }
-        return maxHeap.poll();
+        return maxHeap.remove();
     }
 }
